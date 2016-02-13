0624c697a987bde54cfa1069ff17ec8c7fed761f
iirobpraktikum3@gitlab.ipr.kit.edu <lennart.gilbert@freenet.de>
2016-02-13 14:06:58 +0100

    Implemented bank and group callbacks (0x2100 - 0x2210)

--- canopen_led_node/include/canopen_led_node/canopen_led_layer.h
+++ canopen_led_node/include/canopen_led_node/canopen_led_layer.h
@@ -11,6 +11,7 @@
 
 #include <ros/ros.h>
 #include <ros/package.h>
+#include <std_msgs/Bool.h>
 #include <std_msgs/Int8.h>
 #include <std_msgs/Int16.h>
 #include <std_msgs/Int32.h>
@@ -24,57 +25,6 @@
 #include <std_msgs/String.h>
 #include <std_msgs/Int16MultiArray.h>
 
-class ObjectVariables {
-    const boost::shared_ptr<canopen::ObjectStorage> storage_;
-    struct Getter {
-        boost::shared_ptr<double> val_ptr;
-        boost::function<bool(double&)> func;
-        bool operator ()() { return func(*val_ptr); }
-        template<typename T> Getter(const canopen::ObjectStorage::Entry<T> &entry): func(boost::bind(&Getter::readObject<T>, entry, _1)), val_ptr(new double) { }
-        template<typename T> static bool readObject(canopen::ObjectStorage::Entry<T> &entry, double &res){
-            T val;
-            if(!entry.get(val)) return false;
-            res = val;
-            return true;
-        }
-        operator double*() const { return val_ptr.get(); }
-    };
-    boost::unordered_map<canopen::ObjectDict::Key, Getter> getters_;
-public:
-    template<const uint16_t dt> static double* func(ObjectVariables &list, const canopen::ObjectDict::Key &key){
-        typedef typename canopen::ObjectStorage::DataType<dt>::type type;
-        return list.getters_.insert(std::make_pair(key, Getter(list.storage_->entry<type>(key)))).first->second;
-    }
-    ObjectVariables(const boost::shared_ptr<canopen::ObjectStorage> storage) : storage_(storage) {}
-    bool sync(){
-        bool ok = true;
-        for(boost::unordered_map<canopen::ObjectDict::Key, Getter>::iterator it = getters_.begin(); it != getters_.end(); ++it){
-            ok = it->second() && ok;
-        }
-        return ok;
-    }
-    double * getVariable(const std::string &n) {
-        try{
-            if(n.find("obj") == 0){
-                canopen::ObjectDict::Key key(n.substr(3));
-                boost::unordered_map<canopen::ObjectDict::Key, Getter>::const_iterator it = getters_.find(key);
-                if(it != getters_.end()) return it->second;
-                return canopen::branch_type<ObjectVariables, double * (ObjectVariables &list, const canopen::ObjectDict::Key &k)>(storage_->dict_->get(key)->data_type)(*this, key);
-            }
-        }
-        catch( const std::exception &e){
-            ROS_ERROR_STREAM("Could not find variable '" + n + "', reason: " + boost::diagnostic_information(e));
-        }
-        return 0;
-    }
-};
-
-template<> inline double* ObjectVariables::func<canopen::ObjectDict::DEFTYPE_VISIBLE_STRING >(ObjectVariables &, const canopen::ObjectDict::Key &){ return 0; }
-template<> inline double* ObjectVariables::func<canopen::ObjectDict::DEFTYPE_OCTET_STRING >(ObjectVariables &, const canopen::ObjectDict::Key &){ return 0; }
-template<> inline double* ObjectVariables::func<canopen::ObjectDict::DEFTYPE_UNICODE_STRING >(ObjectVariables &, const canopen::ObjectDict::Key &){ return 0; }
-template<> inline double* ObjectVariables::func<canopen::ObjectDict::DEFTYPE_DOMAIN >(ObjectVariables &, const canopen::ObjectDict::Key &){ return 0; }
-
-
 namespace canopen
 {
   
@@ -86,17 +36,39 @@ class LedLayer : public canopen::Layer{
 
     ros::NodeHandle nh_;
     boost::shared_ptr<canopen::IoBase> base_;
-    ObjectVariables variables_;
+    const boost::shared_ptr<ObjectStorage> storage_;
     
     uint16_t leds_, banks_, bank_size_, groups_ ;
-    canopen::ObjectStorage::Entry<uint8_t> writeDigitalOut8_, bank01_;
-    canopen::ObjectStorage::Entry<int16_t> b01_ch01_, b01_ch02_,b01_ch03_;
-
-protected: 
-  void write(const std_msgs::UInt8::ConstPtr& msg);
-  void setLed(const canopen_led_node::Led::ConstPtr& msg); 
-  void setB1(const std_msgs::Int16MultiArray::ConstPtr& msg); 
+    canopen::ObjectStorage::Entry<uint8_t> globalLedArrayEnable_, writeDigitalOut8_, bankBrightness_,groupBrightness_;
+    canopen::ObjectStorage::Entry<uint16_t> globalBrightness_;
     
+protected: 
+  void writeDigitalOut8(const std_msgs::UInt8::ConstPtr& msg);
+  void setLed(const canopen_led_node::Led::ConstPtr& msg);
+  void setGlobalBrightness(const std_msgs::UInt16::ConstPtr& msg); 
+  void globalLedArrayEnable(const std_msgs::Bool::ConstPtr& msg); 
+  //void setGroupBrightness(const std_msgs::Int16::ConstPtr& msg); 
+
+    
+    //TODO define storage entries
+private:    
+  ros::Subscriber writeDigitalOut8_sub_, set_led_sub_, globalBrightness_sub_, globalLedArrayEnable_sub_; 
+  
+  std::map<int, canopen::ObjectStorage::Entry<uint8_t> > bank_map, group_map;
+  std::map<int, canopen::ObjectStorage::Entry<int16_t> > bankBrightness_map, groupBrightness_map;
+  
+  std::map<int, std::vector< canopen::ObjectStorage::Entry<int16_t> > > led_map;
+  std::map<int, std::vector< canopen::ObjectStorage::Entry<uint16_t> > > channel_map;
+  
+  virtual void handleRead(canopen::LayerStatus &status, const LayerState &current_state);
+  virtual void handleWrite(canopen::LayerStatus &status, const LayerState &current_state);
+  virtual void handleInit(canopen::LayerStatus &status);
+  virtual void handleDiag(canopen::LayerReport &report) { }
+  virtual void handleShutdown(canopen::LayerStatus &status) {  }
+  virtual void handleHalt(canopen::LayerStatus &status) { }
+  virtual void handleRecover(canopen::LayerStatus &status) { handleRead(status, Layer::Ready); }
+  
+      
 public:
     template<typename T> bool set(T & entry, const typename T::type &value) {
 		try {
@@ -109,24 +81,6 @@ public:
     LedLayer(ros::NodeHandle nh,const std::string &name, const boost::shared_ptr<IoBase> & base, const boost::shared_ptr<canopen::ObjectStorage> storage,  XmlRpc::XmlRpcValue & options);
    
 
-    
-    
-    //TODO define storage entries
-private:    
-  ros::Subscriber write_, set_led_, set_B1_; 
-  
-  std::map<int, canopen::ObjectStorage::Entry<uint8_t> > group_map;
-  std::map<int, canopen::ObjectStorage::Entry<uint8_t> > bank_map;
-  std::map<int, std::vector< canopen::ObjectStorage::Entry<int16_t> > > channel_map;
-  
-  virtual void handleRead(canopen::LayerStatus &status, const LayerState &current_state);
-  virtual void handleWrite(canopen::LayerStatus &status, const LayerState &current_state);
-  virtual void handleInit(canopen::LayerStatus &status);
-  virtual void handleDiag(canopen::LayerReport &report) { }
-  virtual void handleShutdown(canopen::LayerStatus &status) {  }
-  virtual void handleHalt(canopen::LayerStatus &status) { }
-  virtual void handleRecover(canopen::LayerStatus &status) { handleRead(status, Layer::Ready); }
-  
   
     
 };
--- canopen_led_node/src/canopen_led_layer.cpp
+++ canopen_led_node/src/canopen_led_layer.cpp
@@ -8,8 +8,7 @@
  */
 using namespace canopen;
 
-void LedLayer::write(const std_msgs::UInt8::ConstPtr& msg) {
-  ROS_INFO("msg: %d", msg->data);
+void LedLayer::writeDigitalOut8(const std_msgs::UInt8::ConstPtr& msg) {
   uint8_t value = msg->data;
   set(writeDigitalOut8_, value);   
 }
@@ -26,46 +25,74 @@ void LedLayer::setLed(const canopen_led_node::Led::ConstPtr& msg) {
   int led = msg->led;
   int data_length = msg->data.size(); 
   if(group != 0) {
-    
+    int group_size = group_map[group].get();
+    if(data_length == 1) {
+      if(led != 0) {
+	//set one channel
+	channel_map[group][led].set(msg->data[0]);
+      } else {
+	//groupBrightness
+	groupBrightness_map[group].set(msg->data[0]);
+      } 
+    } else if(data_length == group_size) {
+      //direct mapping
+      for (int i = 1; i <= group_size; i++) {
+	channel_map[group][i].set(msg->data[i-1]);
+      }
+    }
   } else if(bank != 0) {
     //each channel own value OR one rgb value for bank
-    //bank_map[bank].get() == data_length
-    if(bank_size_ == data_length) {
+    if(data_length == bank_size_) {
+      //direct mapping
       for (int i = 1; i <= bank_size_; i++) {
-	channel_map[bank][i].set(msg->data[i]);
+	led_map[bank][i].set(msg->data[i-1]);
+      } 
+    } else if (data_length == 1) {
+      if(led != 0) {
+	//set one led channel
+	led_map[bank][led].set(msg->data[0]);
+      } else {
+	//bankBrightness
+	bankBrightness_map[bank].set(msg->data[0]);
       }
     } else if (data_length == 3) {
       if(led != 0) {
+	//set all 3 channels of one led
 	for (int i = led; i <= led + 2; i++) {
-	  channel_map[bank][led].set(msg->data[(i%3)-1]);
+	  led_map[bank][led].set(msg->data[(i-1)%3]);
 	}
       } else {
-	for (int i = 1; i <= bank_size_; i++) {
-	channel_map[bank][i].set(msg->data[i%3]);
+	//all leds to same color
+	for (int i = 1; i <= led + 2; i++) {
+	  led_map[bank][led].set(msg->data[(i-1)%3]);
+	}
       }
-      }
-      
-    }
+    }/* else if (data_length >= 3) {
+    if(3 <= data_length ) {
+      for (int i = 1; i <= bank_size_; i++) {
+	led_map[bank][i].set(msg->data[i%3]);
   }
-  
-  
+  }
+  }*/
+  }
 }
 
-void LedLayer::setB1(const std_msgs::Int16MultiArray::ConstPtr& msg) {
-  int dim = msg->layout.dim.size(); 
-  //if(dim == 1) { 
-    set(b01_ch01_, msg->data[0]);   
-    set(b01_ch02_, msg->data[1]); 
-    set(b01_ch03_, msg->data[2]); 
-    
-  //}
+void LedLayer::setGlobalBrightness(const std_msgs::UInt16::ConstPtr& msg) {
+ uint16_t value = msg->data;
+  set(globalBrightness_, value);   
 }
+
+void LedLayer::globalLedArrayEnable(const std_msgs::Bool::ConstPtr& msg) {
+  uint8_t value = msg->data;
+  set(globalLedArrayEnable_, value);  
+} 
+
 
 
 LedLayer::LedLayer(ros::NodeHandle nh,
 		   const std::string &name, const boost::shared_ptr<IoBase> & base, const boost::shared_ptr<ObjectStorage> storage,
 		   XmlRpc::XmlRpcValue & options)
-: Layer(name + " Handle"), base_(base), variables_(storage), nh_(nh) {
+: Layer(name + " Handle"), base_(base), storage_(storage), nh_(nh) {
   
   //number of leds (multiplied by 3 for RGB)
   if(options.hasMember("leds")) leds_ = (const int&) options["leds"]; else leds_ = 0;
@@ -76,31 +103,36 @@ LedLayer::LedLayer(ros::NodeHandle nh,
   //TODO setup storage entries
   
   // Write Outputs 8 Bit
-  //storage->entry(supported_drive_modes_, 0x6200, 0x00); // ro: Number of Output 8 Bit: default=1
   storage->entry(writeDigitalOut8_, 0x6200, 1); // rw: Write Outputs 0x1 to 0x8
+  storage->entry(globalLedArrayEnable_, 0x2006); // 
+  storage->entry(globalBrightness_, 0x2007); // 
+  storage->entry(bankBrightness_, 0x2100 , 0); // 
+  storage->entry(groupBrightness_, 0x2200 , 0); // 
   
-  //setup banks and channels
+  //setup groups, banks and leds
+  for (int i = 1; i <= groups_; i++) {
+    storage->entry(group_map[i], (0x2200 + i), 0);
+    storage->entry(groupBrightness_map[i], 0x2200, i);
+  }  
   
-  
+ 
   for (int i = 1; i <= banks_; i++) {
     //TODO test if working i > 1
     storage->entry(bank_map[i], (0x2100 + i), 0);
-    channel_map[i].resize(bank_size_+ 1);
+    storage->entry(bankBrightness_map[i], 0x2100, i);
+    
+    led_map[i].resize(bank_size_+ 1);
     for (int j = 1; j <= bank_size_; j++) {
-      storage->entry(channel_map[i][j],(0x2100 + i),j);
+      storage->entry(led_map[i][j],(0x2100 + i),j);
     }
   }
   
-  storage->entry(bank01_, 0x2101, 0);
-  storage->entry(b01_ch01_, 0x2101, 1);
-  storage->entry(b01_ch02_, 0x2101, 2);
-  storage->entry(b01_ch03_, 0x2101, 3);
   
   //TODO setup callbacks
-  write_ = nh.subscribe("writeDigitalOut8", 1, &LedLayer::write, this);
-  set_led_ =  nh.subscribe("set_led", 1, &LedLayer::setLed, this);
-  
-  set_B1_ =  nh.subscribe("set_B1", 1, &LedLayer::setB1, this);
+  writeDigitalOut8_sub_ = nh.subscribe("writeDigitalOut8", 1, &LedLayer::writeDigitalOut8, this);
+  set_led_sub_ =  nh.subscribe("set_led", 1, &LedLayer::setLed, this);
+  globalBrightness_sub_ = nh.subscribe("globalBrightness", 1, &LedLayer::setGlobalBrightness, this);
+  globalLedArrayEnable_sub_ = nh.subscribe("globalLedsEnable", 1, &LedLayer::globalLedArrayEnable, this);
 }
 
 
@@ -112,14 +144,12 @@ void LedLayer::handleWrite(LayerStatus &status, const LayerState &current_state)
 }
 void LedLayer::handleInit(LayerStatus &status){
   
-  /*
+  /* init channel_map, needs running cannode for group_size*/  
   for(int i = 1; i <= groups_; i++) {
-      storage->entry(group_map[i], (0x2200 + i), 0);
       int group_size = group_map[i].get();
       channel_map[i].resize(group_size + 1);
       for (int j = 1; j <= group_size; j++) {
-	storage->entry(channel_map[i][j],(0x2200 + i),j);
+	storage_->entry(channel_map[i][j],(0x2200 + i),j);
       }
-  }
-*/  
+  } 
 }
