677cb92b158453f050d78a27112768b80265e7b0
Denis Štogl <denis.stogl@kit.edu>
2018-06-19 14:25:41 +0200

    Update robot_layer_with_fts.cpp

--- canopen_motor_node/src/robot_layer_with_fts.cpp
+++ canopen_motor_node/src/robot_layer_with_fts.cpp
@@ -15,16 +15,7 @@ void RobotLayerWithFTS::handleInit(canopen::LayerStatus &status){
     
     force_torque_control::ForceTorqueControllerHandle handle("canopen_fts_controller_handle");
     
-    ftsh_ = new ForceTorqueSensorHandle(nh_, fts_name, fts_transform_frame);
-    
-    std::vector<std::string> pos_names = position_joint_interface_.getNames();
-    for(size_t i = 0; i < pos_names.size(); i ++)
-        handle.addPosHandle(position_joint_interface_.getHandle(pos_names[i]));
-    ROS_DEBUG_STREAM("add position handle");
-    std::vector<std::string> vel_names = velocity_joint_interface_.getNames();
-    for(size_t i = 0; i < vel_names.size(); i ++)
-        handle.addVelHandle(velocity_joint_interface_.getHandle(vel_names[i]));
-    ROS_DEBUG_STREAM("add velocity handle");
+    ftsh_ = new force_torque_sensor::ForceTorqueSensorHandle(nh_, fts_name, fts_transform_frame);
     
     handle_.addHandle(*ftsh_);
     fts_interface_.registerHandle(handle);
