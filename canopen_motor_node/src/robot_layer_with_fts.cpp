b700a4ecf14c21288fd9e90a9b26b3c8fbbe2edd
andreeatulbure <student@iirob.ipr.kit.edu>
2019-03-12 14:49:09 +0100

    using force_torque_sensor::ForceTorqueSensorHandle

--- canopen_motor_node/src/robot_layer_with_fts.cpp
+++ canopen_motor_node/src/robot_layer_with_fts.cpp
@@ -1,30 +1,17 @@
 #include <canopen_motor_node/robot_layer_with_fts.h>
+#include <canopen_motor_node/robot_layer.h>
 
 using namespace canopen;
 RobotLayerWithFTS::RobotLayerWithFTS(ros::NodeHandle nh) : RobotLayer(nh)
 {
-    nh.param<std::string>("FTS/fts_name", fts_name, "FTS");
+    nh.param<std::string>("FTS/fts_name", fts_name, "ATI_45_Mini");
     nh.param<std::string>("Node/transform_frame", fts_transform_frame, "fts_transform_frame");
 
-    registerInterface(&fts_interface_);
+    registerInterface(&ft_interface_);
 }
 
 void RobotLayerWithFTS::handleInit(canopen::LayerStatus &status){
     RobotLayer::handleInit(status);
-    
-    force_torque_control::ForceTorqueControllerHandle handle("canopen_fts_controller_handle");
-    
     ftsh_ = new force_torque_sensor::ForceTorqueSensorHandle(nh_, fts_name, fts_transform_frame);
-    
-    handle.addHandle(*ftsh_);
-
-    std::vector<std::string> vel_names = vel_interface_.getNames();
-    for(size_t i = 0; i < vel_names.size(); i ++)
-        handle.addVelHandle(vel_interface_.getHandle(vel_names[i]));
-
-    std::vector<std::string> pos_names = pos_interface_.getNames();
-    for(size_t i = 0; i < pos_names.size(); i ++)
-        handle.addPosHandle(pos_interface_.getHandle(pos_names[i]));
-
-    fts_interface_.registerHandle(handle);
+    ft_interface_.registerHandle(*ftsh_);
 }
